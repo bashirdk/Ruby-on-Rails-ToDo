aa1887e7b4dfb96fbdfaa09277a14dcc6250d594
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-03-21 09:58:15 -0400

    Updated messages for tasks and updated redirect for creating and updating tasks

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,7 +28,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
+        format.html { redirect_to todos_path, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TodosController < ApplicationController
   def update
     respond_to do |format|
       if @todo.update(todo_params)
-        format.html { redirect_to @todo, notice: 'Todo was successfully updated.' }
+        format.html { redirect_to todos_path, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class TodosController < ApplicationController
   def destroy
     @todo.destroy
     respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
+      format.html { redirect_to todos_url, notice: 'Task was successfully deleted.' }
       format.json { head :no_content }
     end
   end
