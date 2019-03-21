0444e735429e3c5d5876e1fb0a5a56391600da62
Bashir Duranai-Khan <bashirkdk@gmail.com>
2019-03-20 22:07:05 -0400

    update routes for homepage to point to todo page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :todos
+
+  root to: "todos#index"
+
 end
